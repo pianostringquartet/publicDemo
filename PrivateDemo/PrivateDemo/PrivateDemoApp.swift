12aebe1c62bd8b05d0458808d8253b9b7d3b4929
Chris Clampitt <christian.clampitt@nyu.edu>
2024-06-04 10:53:45 -0600

    initial commit

---
+++ PrivateDemo/PrivateDemo/PrivateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateDemoApp.swift
+//  PrivateDemo
+//
+//  Created by Christian J Clampitt on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PrivateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
